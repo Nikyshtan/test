c9a14866435643c7e82dc2c7803f63764df9ab8f
Nicole Alexandra Denon <nikyalex_denon@hotmail.com.ar>
2021-06-26 20:10:04 -0300

    inicie la aplicacion

---
+++ app.js
@@ -0,0 +1,19 @@
+let port = 8080;
+
+const express = require('express')
+const app = express()
+ 
+//Middleware 
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/hola', function (req, res) {
+    res.send('Hello World')
+  })
+ 
+app.listen(port, function () {
+    console.log("Corriendo en el puerto");
+})
